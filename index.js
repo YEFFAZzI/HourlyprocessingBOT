e51d867f75de7552925392c4ad8ebf7c4e2d5452
YEFFAZzI <42146349+YEFFAZzI@users.noreply.github.com>
2023-06-22 11:27:58 +0000

    123

--- index.js
+++ index.js
@@ -3,40 +3,42 @@ const {Telegraf} = require("telegraf");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const msg = [];
 
+const text = `${msg[0]}:00    110-we-01___${msg[1]} тн.
+120-we-01___${msg[2]}  тн
+120-we-02___${msg[3]} тн.
+130-we-01___${msg[4]} тн. 
+140-we-01___ ${msg[5]} тн.
+Пит.Гц.____________${msg[6]}  м3. 
+Пит.Сорб.__________${msg[7]} м3.
+Пит.Сорб.__________${msg[8]} тн.
+Тех. Вода.__________${msg[9]} м3.`
 
+const a = {
+    a: ''
+}
+bot.start( async (ctx) => {
 
-bot.start( (ctx) => {
-
-    
-  
-    ctx.reply("vvedite cifri")
-   
+    hello
 
 });
 
 
 bot.on('text', async (ctx) => {
+   
     
-    const userMSG = ctx.update.message.text;
+    const userMSG = ctx.message.text;
     
     let msg = userMSG.split('-');
     
+    await ctx.reply(text);
     
-    const text = `${msg[0]}:00    110-we-01___${msg[1]} тн.
-                       120-we-01___${msg[2]}  тн
-                       120-we-02___${msg[3]} тн.
-                       130-we-01___${msg[4]} тн. 
-                       140-we-01___ ${msg[5]} тн.
-              Пит.Гц.____________${msg[6]}  м3. 
-              Пит.Сорб.__________${msg[7]} м3.
-              Пит.Сорб.__________${msg[8]} тн.
-              Тех. Вода.__________${msg[9]} м3.`
-              console.log(msgID1)
-
-   await ctx.editMessageText(ctx.chat.id, msgID1, null, text);
+    
      
     
 })
+
+
+
 
 
 bot.launch();
