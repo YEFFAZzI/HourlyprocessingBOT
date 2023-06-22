ce21cb682ca2104a2dc352ba7f2ebf8f5a0cbb1d
YEFFAZzI <42146349+YEFFAZzI@users.noreply.github.com>
2023-06-22 06:15:03 +0000

    sd

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
-const {Telegraf} = requre("telegraf");
-const bot = new Telegraf(procces.env.BOT_TOKEN);
+const {Telegraf} = require("telegraf");
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start(async (ctx) => ctx.reply("vvedite cifri"));
+bot.start(async (ctx) => await ctx.reply("vvedite cifri"));
+
+
+bot.on('text', (ctx) => ctx.reply('text'))
+
 
 bot.launch();
 
