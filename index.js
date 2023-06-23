90c501e49f726cdff1c156cfb421d6b79cd1183e
YEFFAZzI <42146349+YEFFAZzI@users.noreply.github.com>
2023-06-23 04:15:01 +0000

    qwe

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const {Telegraf} = require("telegraf");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const msg = [];
 
-const text = `${msg[0]}:00    110-we-01___${msg[1]} тн.
+// const text =`${msg[0]}:00    110-we-01___${msg[1]} тн.
+// 120-we-01___${msg[2]}  тн
+// 120-we-02___${msg[3]} тн.
+// 130-we-01___${msg[4]} тн. 
+// 140-we-01___ ${msg[5]} тн.
+// Пит.Гц.____________${msg[6]}  м3. 
+// Пит.Сорб.__________${msg[7]} м3.
+// Пит.Сорб.__________${msg[8]} тн.
+// Тех. Вода.__________${msg[9]} м3.`
+
+const getmsg = (msg) =>
+ `${msg[0]}:00    110-we-01___${msg[1]} тн.
 120-we-01___${msg[2]}  тн
 120-we-02___${msg[3]} тн.
 130-we-01___${msg[4]} тн. 
@@ -12,11 +23,7 @@ const text = `${msg[0]}:00    110-we-01___${msg[1]} тн.
 Пит.Сорб.__________${msg[7]} м3.
 Пит.Сорб.__________${msg[8]} тн.
 Тех. Вода.__________${msg[9]} м3.`
-
-const getmsg = (msg) => text 
     
-
-
 bot.start( async (ctx) => {
 
     hello
@@ -26,18 +33,13 @@ bot.start( async (ctx) => {
 
 bot.on('text', async (ctx) => {
    
-    
     const userMSG = ctx.message.text;
     
     let msg = userMSG.split('-');
     
- 
     await ctx.reply( getmsg(msg));
     await ctx.reply('\n 12-761-989-766-651-651-2528-990-644-786');
-    
-    
-     
-    
+      
 })
 
 
@@ -47,4 +49,4 @@ bot.on('text', async (ctx) => {
 bot.launch();
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
-// '\n 12-761-989-766-651-651-2528-990-644-786'
+
