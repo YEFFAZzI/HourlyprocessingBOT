514b4976bc7d34ebb9f4a52436a9215d75bf887d
YEFFAZzI <42146349+YEFFAZzI@users.noreply.github.com>
2023-06-23 04:09:23 +0000

    v 0.3

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ const text = `${msg[0]}:00    110-we-01___${msg[1]} тн.
 Пит.Сорб.__________${msg[8]} тн.
 Тех. Вода.__________${msg[9]} м3.`
 
+const getmsg = (msg) => text 
+    
+
 
 bot.start( async (ctx) => {
 
@@ -28,17 +31,8 @@ bot.on('text', async (ctx) => {
     
     let msg = userMSG.split('-');
     
-    const text = `${msg[0]}:00    110-we-01___${msg[1]} тн.
-                           120-we-01___${msg[2]}  тн
-                           120-we-02___${msg[3]} тн.
-                           130-we-01___${msg[4]} тн. 
-                           140-we-01___ ${msg[5]} тн.
-                Пит.Гц.____________${msg[6]}  м3. 
-                Пит.Сорб.__________${msg[7]} м3.
-                Пит.Сорб.__________${msg[8]} тн.
-                Тех. Вода.__________${msg[9]} м3.`
-
-    await ctx.reply(text);
+ 
+    await ctx.reply( getmsg(msg));
     await ctx.reply('\n 12-761-989-766-651-651-2528-990-644-786');
     
     
